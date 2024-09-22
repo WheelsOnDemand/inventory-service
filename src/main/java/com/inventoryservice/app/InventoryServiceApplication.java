504e87ee8be1f2bad67999861a46fa1230335f84
shantanumali <shantumali95@gmail.com>
2024-09-22 13:33:56 +0530

    Feignclient changes

--- src/main/java/com/inventoryservice/app/InventoryServiceApplication.java
+++ src/main/java/com/inventoryservice/app/InventoryServiceApplication.java
@@ -3,15 +3,12 @@ package com.inventoryservice.app;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 
 import com.inventoryservice.app.config.ModelMapperManager;
 import com.inventoryservice.app.config.ModelMapperService;
 
 @SpringBootApplication
-@EnableFeignClients
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
